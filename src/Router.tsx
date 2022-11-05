8cb7094b883f537aaea15be0326d70aa11cf6ada
Newton Duarte <newton_@hotmail.com>
2022-11-05 20:24:56 -0300

    feat: add default route layout do app routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
 import { About } from './pages/About'
 import { Home } from './pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/about" element={<About />}></Route>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Route>
     </Routes>
   )
 }
