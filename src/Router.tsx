fca76becaa5db843b591b0f04155dd9e32621f46
Newton Duarte <newton_@hotmail.com>
2022-11-07 06:39:18 -0300

    feat: add login route

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { About } from './pages/About'
 import { Home } from './pages/Home'
+import Login from './pages/Login'
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Route>
     </Routes>
   )
