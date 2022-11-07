22f3845d5d65c3823ea65c65a1081329dc8e23f2
Newton Duarte <newton_@hotmail.com>
2022-11-07 05:55:57 -0300

    feat: create mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { createTheme, ThemeProvider } from '@mui/material'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 
+const theme = createTheme()
+
 export function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
