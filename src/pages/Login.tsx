2be689a1ad088fe109366fbc943e5dfd8b78d6bc
Newton Duarte <newton_@hotmail.com>
2022-11-09 06:45:48 -0300

    feat: add login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,14 @@
 import {
   Box,
+  Button,
   Container,
-  FormGroup,
+  Divider,
+  FormControl,
   Paper,
   TextField,
   Typography,
 } from '@mui/material'
+import { NavLink } from 'react-router-dom'
 
 export default function Login() {
   return (
@@ -15,17 +18,42 @@ export default function Login() {
         height: 'calc(100vh - 64px)',
         display: 'flex',
         alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
-      <Paper sx={{ width: 300 }}>
-        <Typography variant="h6">Login</Typography>
-        <Box component="form">
-          <FormGroup>
+      <Paper
+        sx={{
+          width: {
+            xs: '100%',
+            sm: 400,
+          },
+          p: 3,
+        }}
+      >
+        <Typography variant="h5" sx={{ mb: 4, textAlign: 'center' }}>
+          Ignite Deliveries - Login
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { mb: 3 },
+          }}
+        >
+          <FormControl fullWidth>
             <TextField id="email" type="email" label="E-mail" />
-          </FormGroup>
-          <FormGroup>
+          </FormControl>
+          <FormControl fullWidth>
             <TextField id="password" type="password" label="Senha" />
-          </FormGroup>
+          </FormControl>
+          <FormControl fullWidth>
+            <Button variant="contained" size="large">
+              Entrar
+            </Button>
+            <Divider sx={{ my: 3 }} />
+            <Button component={NavLink} to="/sign-up" size="large">
+              Fazer cadastro
+            </Button>
+          </FormControl>
         </Box>
       </Paper>
     </Container>
