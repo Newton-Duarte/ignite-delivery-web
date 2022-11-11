745b30833e880df9fb4b9a5608bf752eacf9aeff
Newton Duarte <newton_@hotmail.com>
2022-11-11 06:34:00 -0300

    feat: add home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,60 @@
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material'
+import { Box, Container } from '@mui/system'
+
 export function Home() {
-  return <h1>Homepage</h1>
+  return (
+    <Container maxWidth="xl" sx={{ my: 5 }}>
+      <Box>
+        <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 3 }}>
+          Entregas disponíveis
+        </Typography>
+      </Box>
+      <TableContainer
+        component={Paper}
+        sx={{
+          maxHeight: {
+            xs: 640,
+            sm: 740,
+          },
+        }}
+      >
+        <Table aria-label="available-deliveries-table" stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ fontWeight: 'bold' }}>Solicitante</TableCell>
+              <TableCell sx={{ fontWeight: 'bold' }}>Encomenda</TableCell>
+              <TableCell sx={{ fontWeight: 'bold', width: '25%' }}>
+                Endereço
+              </TableCell>
+              <TableCell sx={{ fontWeight: 'bold', width: '25%' }}>
+                Ações
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {[...Array(30).keys()].map((n) => (
+              <TableRow key={n}>
+                <TableCell>John Doe</TableCell>
+                <TableCell>Coxinha</TableCell>
+                <TableCell>Rua Ali Perto, 123 - Barrio Cidade/ST</TableCell>
+                <TableCell>
+                  <Button variant="contained">Realizar Entrega</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  )
 }
