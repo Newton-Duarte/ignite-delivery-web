c7b62928d03d80f73a7d65c51e9bf40b1d9ffb1d
Newton Duarte <newton_@hotmail.com>
2022-11-12 20:43:39 -0300

    feat: add loading state and list loading component to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,8 +10,16 @@ import {
   Typography,
 } from '@mui/material'
 import { Box, Container } from '@mui/system'
+import { useEffect, useState } from 'react'
+import ListLoading from '../components/ListLoading'
 
 export function Home() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 800)
+  }, [])
+
   return (
     <Container maxWidth="xl" sx={{ my: 5 }}>
       <Box>
@@ -28,32 +36,36 @@ export function Home() {
           },
         }}
       >
-        <Table aria-label="available-deliveries-table" stickyHeader>
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{ fontWeight: 'bold' }}>Solicitante</TableCell>
-              <TableCell sx={{ fontWeight: 'bold' }}>Encomenda</TableCell>
-              <TableCell sx={{ fontWeight: 'bold', width: '25%' }}>
-                Endereço
-              </TableCell>
-              <TableCell sx={{ fontWeight: 'bold', width: '25%' }}>
-                Ações
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {[...Array(30).keys()].map((n) => (
-              <TableRow key={n}>
-                <TableCell>John Doe</TableCell>
-                <TableCell>Coxinha</TableCell>
-                <TableCell>Rua Ali Perto, 123 - Barrio Cidade/ST</TableCell>
-                <TableCell>
-                  <Button variant="contained">Realizar Entrega</Button>
+        {loading ? (
+          <ListLoading />
+        ) : (
+          <Table aria-label="available-deliveries-table" stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ fontWeight: 'bold' }}>Solicitante</TableCell>
+                <TableCell sx={{ fontWeight: 'bold' }}>Encomenda</TableCell>
+                <TableCell sx={{ fontWeight: 'bold', width: '25%' }}>
+                  Endereço
+                </TableCell>
+                <TableCell sx={{ fontWeight: 'bold', width: '25%' }}>
+                  Ações
                 </TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHead>
+            <TableBody>
+              {[...Array(30).keys()].map((n) => (
+                <TableRow key={n}>
+                  <TableCell>John Doe</TableCell>
+                  <TableCell>Coxinha</TableCell>
+                  <TableCell>Rua Ali Perto, 123 - Barrio Cidade/ST</TableCell>
+                  <TableCell>
+                    <Button variant="contained">Realizar Entrega</Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
       </TableContainer>
     </Container>
   )
