4e46d58a43b1a214ac45d0f33f08f93f2d01cf2b
Newton Duarte <newton_@hotmail.com>
2022-11-14 16:43:48 -0300

    feat: add snackbar component to default layout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,11 +1,25 @@
+import { Snackbar } from '@mui/material'
 import { Outlet } from 'react-router-dom'
 import { Header } from '../components/Header'
+import { useSnackbar } from '../contexts/snackbar'
 
 export function DefaultLayout() {
+  const { isSnackbarOpen, closeSnackbar, snackbarMessage } = useSnackbar()
+
   return (
     <div>
       <Header />
       <Outlet />
+      <Snackbar
+        open={isSnackbarOpen}
+        onClose={closeSnackbar}
+        autoHideDuration={6000}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        message={snackbarMessage}
+      />
     </div>
   )
 }
