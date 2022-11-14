f75a1faebb409bf8a22e3bebfc6a031009a4af52
Newton Duarte <newton_@hotmail.com>
2022-11-14 16:42:34 -0300

    feat: add user profile and deliveries pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,7 +3,9 @@ import { DefaultLayout } from './layouts/DefaultLayout'
 import { About } from './pages/About'
 import { Home } from './pages/Home'
 import Login from './pages/Login'
+import Profile from './pages/Profile'
 import SignUp from './pages/SignUp'
+import { UserDeliveries } from './pages/UserDeliveries'
 
 export function Router() {
   return (
@@ -13,6 +15,8 @@ export function Router() {
         <Route path="/about" element={<About />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/user/profile" element={<Profile />}></Route>
+        <Route path="/user/deliveries" element={<UserDeliveries />}></Route>
       </Route>
     </Routes>
   )
