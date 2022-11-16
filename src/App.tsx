3ca6a577d5a3c08c9a32bc693dd7dcdd18d7f1b3
Newton Duarte <newton_@hotmail.com>
2022-11-16 06:46:24 -0300

    feat: add auth provider to app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import { BrowserRouter } from 'react-router-dom'
 import { createTheme, ThemeProvider } from '@mui/material'
 import { SnackbarProvider } from './contexts/snackbar'
 import { Router } from './Router'
+import { AuthProvider } from './contexts/AuthContext'
 
 const theme = createTheme()
 
 export function App() {
   return (
-    <SnackbarProvider>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-      </ThemeProvider>
-    </SnackbarProvider>
+    <AuthProvider>
+      <SnackbarProvider>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </ThemeProvider>
+      </SnackbarProvider>
+    </AuthProvider>
   )
 }
