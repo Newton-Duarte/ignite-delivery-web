910deb18e28e0fd398820727e8ca208e4660e7b2
Newton Duarte <newton_@hotmail.com>
2022-11-17 06:43:07 -0300

    feat: add sign in sign out and user state

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,16 +1,34 @@
-import { createContext, ReactNode, useContext } from 'react'
+/* eslint-disable dot-notation */
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
+import { useNavigate } from 'react-router-dom'
 import { api } from '../services/api'
+import { useSnackbar } from './snackbar'
+
+const LOCALSTORAGE_USER_KEY = 'ignitedelivery.user'
+const LOCALSTORAGE_TOKEN_KEY = 'ignitedelivery.token'
 
 interface User {
   id: string
   name: string
-  email: string
+  username: string
   address?: string
 }
 
-interface AuthContextData {
+type SignInResponse = {
   user: User
-  signIn: (email: string, password: string) => void
+  token: string
+}
+
+interface AuthContextData {
+  user: User | undefined
+  signIn: (username: string, password: string) => void
+  isAuthenticated: boolean
 }
 
 interface AuthProviderProps {
@@ -26,20 +44,62 @@ export function useAuth() {
 }
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  async function signIn(email: string, password: string) {
+  const [currentUser, setCurrentUser] = useState<User>()
+  const isAuthenticated = !!currentUser
+
+  const navigate = useNavigate()
+  const { showSnackbarMessage } = useSnackbar()
+
+  useEffect(() => {
+    const token = localStorage.getItem(LOCALSTORAGE_TOKEN_KEY)
+
+    if (token) {
+      api
+        .get('me')
+        .then((response) => {
+          const { user } = response.data
+
+          setCurrentUser({ ...user })
+          api.defaults.headers['Authorization'] = `Bearer ${token}`
+        })
+        .catch(() => {
+          signOut()
+        })
+    }
+  }, [])
+
+  async function signIn(username: string, password: string) {
     try {
-      const response = await api.post('/clients/authenticate', {
-        email,
+      const response = await api.post<SignInResponse>('/clients/authenticate', {
+        username,
         password,
       })
-      console.log(response)
+
+      const { user, token } = response.data
+
+      localStorage.setItem(LOCALSTORAGE_USER_KEY, JSON.stringify(user))
+      localStorage.setItem(LOCALSTORAGE_TOKEN_KEY, token)
+
+      setCurrentUser({ ...user })
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
+      showSnackbarMessage('Login efetuado com sucesso')
+
+      navigate('/')
     } catch (error) {
-      console.log(error)
+      showSnackbarMessage('Usuário ou senha inválidos')
     }
   }
 
+  function signOut() {
+    localStorage.clear()
+    navigate('/login')
+  }
+
   return (
-    <AuthContext.Provider value={{ user: {} as User, signIn }}>
+    <AuthContext.Provider
+      value={{ user: currentUser, signIn, isAuthenticated }}
+    >
       {children}
     </AuthContext.Provider>
   )
