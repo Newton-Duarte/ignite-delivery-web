73e256d2ef90b2adb2e2da446791845e5de7c8b5
Newton Duarte <newton_@hotmail.com>
2022-11-23 07:23:10 -0300

    feat: implement deliveries list component with loading skeleton

--- src/components/DeliveriesList.tsx
+++ src/components/DeliveriesList.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import {
   Table,
   TableHead,
@@ -5,43 +6,92 @@ import {
   TableCell,
   TableBody,
   Button,
+  Paper,
+  Typography,
+  TableContainer,
 } from '@mui/material'
 import { useDeliveries } from '../contexts/DeliveriesContext'
+import noDataSvg from '../assets/undraw_no_data.svg'
+import ListLoading from './ListLoading'
+import { useAuth } from '../contexts/AuthContext'
 
 export default function DeliveriesList() {
-  const { deliveries } = useDeliveries()
+  const { fetchDeliveries, deliveries, loading } = useDeliveries()
+  const { isDeliveryman } = useAuth()
+
+  useEffect(() => {
+    fetchDeliveries()
+  }, [])
 
   return (
     <>
-      {deliveries?.length ? (
-        <Table aria-label="available-deliveries-table" stickyHeader>
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{ fontWeight: 'bold' }}>Solicitante</TableCell>
-              <TableCell sx={{ fontWeight: 'bold' }}>Encomenda</TableCell>
-              <TableCell sx={{ fontWeight: 'bold', width: '25%' }}>
-                Endereço
-              </TableCell>
-              <TableCell sx={{ fontWeight: 'bold', width: '25%' }}>
-                Ações
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {deliveries.map((delivery) => (
-              <TableRow key={delivery.id}>
-                <TableCell>{delivery.client}</TableCell>
-                <TableCell>{delivery.item_name}</TableCell>
-                <TableCell>{delivery.address}</TableCell>
-                <TableCell>
-                  <Button variant="contained">Realizar Entrega</Button>
+      {!deliveries?.length || loading ? (
+        <ListLoading />
+      ) : deliveries?.length ? (
+        <TableContainer
+          component={Paper}
+          sx={{
+            maxHeight: {
+              xs: 640,
+              sm: 740,
+            },
+          }}
+        >
+          <Table aria-label="available-deliveries-table" stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ fontWeight: 'bold' }}>Solicitante</TableCell>
+                <TableCell sx={{ fontWeight: 'bold' }}>Encomenda</TableCell>
+                <TableCell sx={{ fontWeight: 'bold', width: '25%' }}>
+                  Endereço
                 </TableCell>
+                <TableCell sx={{ fontWeight: 'bold', width: '25%' }}>
+                  Criado em
+                </TableCell>
+                {isDeliveryman && (
+                  <TableCell sx={{ fontWeight: 'bold', width: '25%' }}>
+                    Ações
+                  </TableCell>
+                )}
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHead>
+            <TableBody>
+              {deliveries.map((delivery) => (
+                <TableRow key={delivery.id}>
+                  <TableCell>{delivery.client.name}</TableCell>
+                  <TableCell>{delivery.item_name}</TableCell>
+                  <TableCell>{delivery.address}</TableCell>
+                  <TableCell>
+                    {new Intl.DateTimeFormat().format(
+                      new Date(delivery.created_at),
+                    )}
+                  </TableCell>
+                  {isDeliveryman && (
+                    <TableCell>
+                      <Button variant="contained">Realizar Entrega</Button>
+                    </TableCell>
+                  )}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       ) : (
-        <p>Lista vazia</p>
+        <Paper
+          sx={{
+            width: {
+              xs: '100%',
+              sm: 400,
+            },
+            p: 5,
+            margin: '0 auto',
+          }}
+        >
+          <img src={noDataSvg} alt="empty data" style={{ maxWidth: '100%' }} />
+          <Typography variant="body1" sx={{ mt: 2, textAlign: 'center' }}>
+            Nenhuma entrega encontrada
+          </Typography>
+        </Paper>
       )}
     </>
   )
