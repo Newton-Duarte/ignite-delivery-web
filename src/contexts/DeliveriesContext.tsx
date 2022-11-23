ef58389b2918beb437336ae3eced86a7b12c7b3f
Newton Duarte <newton_@hotmail.com>
2022-11-23 07:24:41 -0300

    refactor: deliveries context to not fetch deliveries initially and export a fetch function

--- src/contexts/DeliveriesContext.tsx
+++ src/contexts/DeliveriesContext.tsx
@@ -1,28 +1,44 @@
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useEffect,
-  useState,
-} from 'react'
+import { createContext, ReactNode, useContext, useState } from 'react'
+import { useLoading } from '../hooks/useLoading'
 import { api } from '../services/api'
+import { useSnackbar } from './snackbar'
 
 interface DeliveriesProviderProps {
   children: ReactNode
 }
 
+interface Client {
+  id: string
+  name: string
+  address: string
+}
+
+interface Deliveryman {
+  id: string
+  name: string
+}
+
 interface Delivery {
   id: string
-  client: string
+  client: Client
   item_name: string
   address?: string
-  deliveryman?: string
+  deliveryman?: Deliveryman
   created_at: string
   end_at?: string
 }
 
+interface DeliveryFormData {
+  item_name: string
+  address: string
+}
+
 interface DeliveriesContextData {
   deliveries: Delivery[]
+  loading: boolean
+  loadingCreateDelivery: boolean
+  fetchDeliveries: () => void
+  createDelivery: (data: DeliveryFormData, onSuccess: () => void) => void
 }
 
 const DeliveriesContext = createContext({} as DeliveriesContextData)
@@ -35,16 +51,55 @@ export function useDeliveries() {
 
 export function DeliveriesProvider({ children }: DeliveriesProviderProps) {
   const [deliveries, setDeliveries] = useState<Delivery[]>([])
+  const { loading, setLoading } = useLoading(false)
+  const {
+    loading: loadingCreateDelivery,
+    setLoading: setLoadingCreateDelivery,
+  } = useLoading(false)
 
-  useEffect(() => {
-    api
-      .get('/deliveries')
-      .then((response) => setDeliveries(response.data))
-      .catch((error) => console.log(error))
-  }, [])
+  const { showSnackbarMessage } = useSnackbar()
+
+  async function fetchDeliveries() {
+    setLoading(true)
+
+    try {
+      const response = await api.get('/deliveries')
+      setDeliveries(response.data)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      showSnackbarMessage('Erro ao listar entregas disponíveis')
+      setLoading(false)
+    }
+  }
+
+  async function createDelivery(data: DeliveryFormData, onSuccess: () => void) {
+    setLoadingCreateDelivery(true)
+
+    try {
+      const response = await api.post('/deliveries', data)
+
+      setDeliveries([...deliveries, response.data])
+      showSnackbarMessage('Entrega adicionada com sucesso!')
+      setLoadingCreateDelivery(false)
+      onSuccess()
+    } catch (error) {
+      console.log(error)
+      showSnackbarMessage('Ocorreu um erro ao adicionar a entrega.')
+      setLoadingCreateDelivery(false)
+    }
+  }
 
   return (
-    <DeliveriesContext.Provider value={{ deliveries }}>
+    <DeliveriesContext.Provider
+      value={{
+        deliveries,
+        loading,
+        loadingCreateDelivery,
+        fetchDeliveries,
+        createDelivery,
+      }}
+    >
       {children}
     </DeliveriesContext.Provider>
   )
