364ede2673383285f7c0b67486c83e887b9db895
Newton Duarte <newton_@hotmail.com>
2022-11-23 07:22:51 -0300

    feat: implement create delivery modal

--- src/components/CreateDelivery.tsx
+++ src/components/CreateDelivery.tsx
@@ -10,19 +10,44 @@ import {
   Grid,
   Box,
 } from '@mui/material'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { useDeliveries } from '../contexts/DeliveriesContext'
+
+const createDeliveryFormValidationSchema = zod.object({
+  item_name: zod.string().min(2, 'Informe a encomenda'),
+  address: zod.string().min(5, 'Informe o endereço'),
+})
+
+type CreateDeliveryFormData = zod.infer<
+  typeof createDeliveryFormValidationSchema
+>
 
 interface CreateDeliveryProps {
   isOpen: boolean
   onClose: () => void
-  onSave: (data: any) => void
 }
 
 export default function CreateDelivery({
   isOpen,
   onClose,
-  onSave,
 }: CreateDeliveryProps) {
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, reset } = useForm<CreateDeliveryFormData>({
+    resolver: zodResolver(createDeliveryFormValidationSchema),
+    defaultValues: {
+      item_name: '',
+      address: '',
+    },
+  })
+
+  const { createDelivery, loadingCreateDelivery } = useDeliveries()
+
+  function onSave(data: CreateDeliveryFormData) {
+    createDelivery(data, () => {
+      reset()
+      onClose()
+    })
+  }
 
   return (
     <Dialog
@@ -41,6 +66,8 @@ export default function CreateDelivery({
                 id="item_name"
                 type="text"
                 label="Encomenda"
+                autoFocus
+                disabled={loadingCreateDelivery}
               />
             </FormControl>
             <FormControl component={Grid} item fullWidth>
@@ -49,15 +76,24 @@ export default function CreateDelivery({
                 id="address"
                 type="text"
                 label="Endereço"
+                disabled={loadingCreateDelivery}
               />
             </FormControl>
           </Grid>
         </DialogContent>
         <DialogActions sx={{ px: 3, py: 2 }}>
-          <Button variant="text" onClick={onClose}>
+          <Button
+            variant="text"
+            onClick={onClose}
+            disabled={loadingCreateDelivery}
+          >
             Cancelar
           </Button>
-          <Button type="submit" variant="contained" onClick={onSave} autoFocus>
+          <Button
+            type="submit"
+            variant="contained"
+            disabled={loadingCreateDelivery}
+          >
             Salvar
           </Button>
         </DialogActions>
