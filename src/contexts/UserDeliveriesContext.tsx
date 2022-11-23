38aa6c9085f6e4d7d61ca922e97775d5cb9ca966
Newton Duarte <newton_@hotmail.com>
2022-11-23 07:25:08 -0300

    feat: implement user deliveries context with fetch user deliveries

--- src/contexts/UserDeliveriesContext.tsx
+++ src/contexts/UserDeliveriesContext.tsx
@@ -5,24 +5,39 @@ import {
   useEffect,
   useState,
 } from 'react'
+import { useLoading } from '../hooks/useLoading'
 import { api } from '../services/api'
+import { useAuth } from './AuthContext'
+import { useSnackbar } from './snackbar'
 
 interface UserDeliveriesProviderProps {
   children: ReactNode
 }
 
-interface Delivery {
+interface Client {
   id: string
-  client: string
+  name: string
+  address: string
+}
+
+interface Deliveryman {
+  id: string
+  name: string
+}
+
+export interface Delivery {
+  id: string
+  client: Client
   item_name: string
   address?: string
-  deliveryman?: string
+  deliveryman?: Deliveryman
   created_at: string
   end_at?: string
 }
 
 interface UserDeliveriesContextData {
   deliveries: Delivery[]
+  loading: boolean
 }
 
 const LOCAL_STORAGE_LOGIN_TYPE_KEY = 'ignitedelivery.logintype'
@@ -38,9 +53,15 @@ export function useUserDeliveries() {
 export function UserDeliveriesProvider({
   children,
 }: UserDeliveriesProviderProps) {
+  const { isAuthenticated } = useAuth()
   const [deliveries, setUserDeliveries] = useState<Delivery[]>([])
+  const { loading, setLoading } = useLoading(true)
+
+  const { showSnackbarMessage } = useSnackbar()
 
   useEffect(() => {
+    if (!isAuthenticated) return
+
     const loginType = localStorage.getItem(LOCAL_STORAGE_LOGIN_TYPE_KEY)
     const URL =
       loginType === 'customer'
@@ -49,12 +70,19 @@ export function UserDeliveriesProvider({
 
     api
       .get(URL)
-      .then((response) => setUserDeliveries(response.data))
-      .catch((error) => console.log(error))
-  }, [])
+      .then((response) => {
+        setUserDeliveries(response.data)
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.log(error)
+        showSnackbarMessage('Erro ao listar entregas do usuário')
+        setLoading(false)
+      })
+  }, [isAuthenticated])
 
   return (
-    <UserDeliveriesContext.Provider value={{ deliveries }}>
+    <UserDeliveriesContext.Provider value={{ deliveries, loading }}>
       {children}
     </UserDeliveriesContext.Provider>
   )
