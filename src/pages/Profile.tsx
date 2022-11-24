24bfe325f4ed9bf6cb75daf70c0ae88c865cc969
Newton Duarte <newton_@hotmail.com>
2022-11-24 06:37:24 -0300

    feat: add login type on profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -6,12 +6,15 @@ import {
   Divider,
   Box,
 } from '@mui/material'
+import { grey } from '@mui/material/colors'
 import UserDeliveriesList from '../components/UserDeliveriesList'
 import { useAuth } from '../contexts/AuthContext'
 import { UserDeliveriesProvider } from '../contexts/UserDeliveriesContext'
 
 export default function Profile() {
-  const { user } = useAuth()
+  const { user, isClient } = useAuth()
+
+  const loginTypeColor = grey[500]
 
   return (
     <Container
@@ -45,8 +48,15 @@ export default function Profile() {
           {user?.name.split(' ')[0][0]}
           {user?.name.split(' ')[1][0]}
         </Avatar>
-        <Typography variant="h5" sx={{ mt: 2, mb: 4, textAlign: 'center' }}>
+        <Typography variant="h5" sx={{ mt: 2, textAlign: 'center' }}>
           {user?.name}
+        </Typography>
+        <Typography
+          variant="body2"
+          color={loginTypeColor}
+          sx={{ mb: 4, textAlign: 'center' }}
+        >
+          {isClient ? 'Cliente' : 'Entregador'}
         </Typography>
         <Divider />
         <Box>
