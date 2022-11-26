7c845f83ba52d22c478ca8c0df4ae53a6d996d24
Newton Duarte <newton_@hotmail.com>
2022-11-26 10:31:55 -0300

    refactor: change home page to make requests and control url parameters

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,41 @@
-import { Button, Typography } from '@mui/material'
-import { Box, Container } from '@mui/system'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { Box, Button, Container, Paper, Typography } from '@mui/material'
 import CreateDelivery from '../components/CreateDelivery'
 import DeliveriesList from '../components/DeliveriesList'
+import ListLoading from '../components/ListLoading'
 import { useAuth } from '../contexts/AuthContext'
-import { DeliveriesProvider } from '../contexts/DeliveriesContext'
+import { useDeliveries } from '../contexts/DeliveriesContext'
+import noDataSvg from '../assets/undraw_no_data.svg'
+import { useSearchParams } from 'react-router-dom'
 
 export function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false)
 
+  const { fetchDeliveries, deliveries, loading } = useDeliveries()
+
   const { isClient } = useAuth()
+
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const page = Number(searchParams.get('page')) || 1
+  const perPage = Number(searchParams.get('per_page')) || 5
+
+  useEffect(() => {
+    if (loading) return
+
+    fetchDeliveries({ page, perPage })
+  }, [page, perPage])
+
+  function handleChangePage(newPage: number) {
+    setSearchParams({
+      per_page: String(perPage),
+      page: String(newPage + 1),
+    })
+  }
+
+  function handleChangePerPage(newPerPage: number) {
+    setSearchParams({ page: '1', per_page: String(newPerPage) })
+  }
 
   return (
     <Container maxWidth="xl" sx={{ my: 5 }}>
@@ -30,13 +56,38 @@ export function Home() {
           </Button>
         )}
       </Box>
-      <DeliveriesProvider>
-        <DeliveriesList />
-        <CreateDelivery
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
+      {!deliveries?.total || loading ? (
+        <ListLoading />
+      ) : deliveries?.total ? (
+        <DeliveriesList
+          deliveries={deliveries.data}
+          total={deliveries.total}
+          page={page - 1}
+          perPage={perPage}
+          onChangePage={handleChangePage}
+          onChangePerPage={handleChangePerPage}
         />
-      </DeliveriesProvider>
+      ) : (
+        <Paper
+          sx={{
+            width: {
+              xs: '100%',
+              sm: 400,
+            },
+            p: 5,
+            margin: '0 auto',
+          }}
+        >
+          <img src={noDataSvg} alt="empty data" style={{ maxWidth: '100%' }} />
+          <Typography variant="body1" sx={{ mt: 2, textAlign: 'center' }}>
+            Nenhuma entrega encontrada
+          </Typography>
+        </Paper>
+      )}
+      <CreateDelivery
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
     </Container>
   )
 }
