9206c948ddc85d454a773acdd03c79b1abd7663e
Newton Duarte <newton_@hotmail.com>
2022-11-28 06:22:27 -0300

    feat: add auth and private routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
+import AuthRoutes from './AuthRoutes'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { About } from './pages/About'
 import Home from './pages/HomeContainer'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
 import SignUp from './pages/SignUp'
+import { PrivateRoutes } from './PrivateRoutes'
 
 export function Router() {
   return (
@@ -12,9 +14,13 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/sign-up" element={<SignUp />}></Route>
-        <Route path="/user/profile" element={<Profile />}></Route>
+        <Route element={<PrivateRoutes />}>
+          <Route path="/user/profile" element={<Profile />}></Route>
+        </Route>
+        <Route element={<AuthRoutes />}>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+        </Route>
       </Route>
     </Routes>
   )
