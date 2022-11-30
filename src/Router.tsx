22e1c74f65f94f6eb639cadf8eacbbb59895643d
Newton Duarte <newton_@hotmail.com>
2022-11-30 06:02:20 -0300

    feat: show loading when auth is initializing

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,34 @@
+import { Box } from '@mui/material'
 import { Route, Routes } from 'react-router-dom'
 import AuthRoutes from './AuthRoutes'
+import { Loading } from './components/Loading'
+import { useAuth } from './contexts/AuthContext'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { About } from './pages/About'
 import Home from './pages/HomeContainer'
 import Login from './pages/Login'
-import Profile from './pages/Profile'
+import Profile from './pages/ProfileContainer'
 import SignUp from './pages/SignUp'
 import { PrivateRoutes } from './PrivateRoutes'
 
 export function Router() {
+  const { user } = useAuth()
+
+  if (user === undefined) {
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+        }}
+      >
+        <Loading />
+      </Box>
+    )
+  }
+
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
