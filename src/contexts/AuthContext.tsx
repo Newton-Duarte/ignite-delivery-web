b61346c5710c8e7d9866e706446ff82583038a09
Newton Duarte <newton_@hotmail.com>
2022-11-30 06:04:29 -0300

    fix: sign out

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -165,7 +165,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }
 
   function signOut() {
-    setCurrentUser(undefined)
+    setCurrentUser(null)
     localStorage.clear()
     navigate('/login')
   }
